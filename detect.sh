fa0bca1d7d11d32779aad59775309c249487a686
vagrantxiao <yuanlongxiao24@gmail.com>
2020-11-26 02:37:15 +0000

    all files

---
+++ detect.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_conv_wrapper_prj/bin_conv_wrapper/syn/verilog ./bin_conv_wrapper -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_conv_wt_gen_0_prj/bin_conv_wt_gen_0/syn/verilog ./bin_conv_wt_gen_0 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_conv_wt_gen_1_prj/bin_conv_wt_gen_1/syn/verilog ./bin_conv_wt_gen_1 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_conv_wt_gen_2_prj/bin_conv_wt_gen_2/syn/verilog ./bin_conv_wt_gen_2 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_conv_wt_gen_3_prj/bin_conv_wt_gen_3/syn/verilog ./bin_conv_wt_gen_3 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_dense_wrapper_prj/bin_dense_wrapper/syn/verilog ./bin_dense_wrapper -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_dense_wt_gen_0_prj/bin_dense_wt_gen_0/syn/verilog ./bin_dense_wt_gen_0 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_dense_wt_gen_10_prj/bin_dense_wt_gen_10/syn/verilog ./bin_dense_wt_gen_10 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_dense_wt_gen_1_prj/bin_dense_wt_gen_1/syn/verilog ./bin_dense_wt_gen_1 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_dense_wt_gen_2_prj/bin_dense_wt_gen_2/syn/verilog ./bin_dense_wt_gen_2 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_dense_wt_gen_3_prj/bin_dense_wt_gen_3/syn/verilog ./bin_dense_wt_gen_3 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_dense_wt_gen_4_prj/bin_dense_wt_gen_4/syn/verilog ./bin_dense_wt_gen_4 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_dense_wt_gen_5_prj/bin_dense_wt_gen_5/syn/verilog ./bin_dense_wt_gen_5 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_dense_wt_gen_6_prj/bin_dense_wt_gen_6/syn/verilog ./bin_dense_wt_gen_6 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_dense_wt_gen_7_prj/bin_dense_wt_gen_7/syn/verilog ./bin_dense_wt_gen_7 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_dense_wt_gen_8_prj/bin_dense_wt_gen_8/syn/verilog ./bin_dense_wt_gen_8 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/bin_dense_wt_gen_9_prj/bin_dense_wt_gen_9/syn/verilog ./bin_dense_wt_gen_9 -rf 
+cp ../MeshINT/pr_flow_wide_width/workspace/F002_hls_bnn2/fp_conv_prj/fp_conv/syn/verilog ./fp_conv -rf 
